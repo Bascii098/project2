c3e2490ae3787d88e02cb8fc70330582b7b47573
Bascii098 <2021210202@bupt.edu.cn>
2024-06-18 00:28:06 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
-
+import{getCategoryAPI}from "@/apis/test"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+getCategoryAPI().then(res=>{
+  console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
