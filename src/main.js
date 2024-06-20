7c320f34595256c5da8c779df1c13bc5e9943aa7
Bascii098 <2021210202@bupt.edu.cn>
2024-06-20 23:19:47 +0800

    product渲染

--- src/main.js
+++ src/main.js
@@ -2,24 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/styles/common.scss'
 import App from './App.vue'
-import { useIntersectionObserver } from '@vueuse/core'
+import{LazyPlugin}from '@/directives/lazyLoad'
 
 import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(LazyPlugin)
 app.mount('#app')
-app.directive('img-lazy',{
-  mounted(el,binding){
- useIntersectionObserver(
-  el,
-  ([{ isIntersecting }], ) => {
-   if(isIntersecting){
-    el.src=binding.value
-   }
-  },
-)
-  }
-})
+
