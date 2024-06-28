b74a68824c6a16c6ac2ee2d03d0064f39075e9a6
Bascii098 <2021210202@bupt.edu.cn>
2024-06-29 01:06:10 +0800

    分级列表显示

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@ import { getCategoryAPI } from "@/apis/category";
 import{ getBannerAPI } from '@/apis/bannerList'
 import {ref,onMounted} from 'vue'
 import{useRoute} from'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
 const BannerList = ref([])
   const getBanner=async()=> {
    const res = await getBannerAPI({
@@ -39,6 +40,26 @@ const route =useRoute()
       <img :src="item.imgUrl" alt="">
     </el-carousel-item>
   </el-carousel>
+</div>
+<!-- 分类页 -->
+<div class="sub-list">
+  <h3>全部分类</h3>
+  <ul>
+    <li v-for="i in categoryData.children" :key="i.id">
+      <RouterLink to="/">
+        <img :src="i.picture" />
+        <p>{{ i.name }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+</div>
+<div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+  <div class="head">
+    <h3>- {{ item.name }}-</h3>
+  </div>
+  <div class="body">
+    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+  </div>
 </div>
     </div>
   </div>
