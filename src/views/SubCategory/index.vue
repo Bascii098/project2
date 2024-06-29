270e4a83cabaa42b4b3d6d3627dabcdd49af37d4
Bascii098 <2021210202@bupt.edu.cn>
2024-06-30 01:10:02 +0800

    详情页配置

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -23,6 +23,24 @@ const getGoodsData=async()=>{
   goodsList.value=res.result.items
   }
   onMounted(()=>{getGoodsData()})
+
+  const tabChange = () => {
+  reqData.value.page = 1
+  getGoodsData()
+}
+
+// 加载更多
+const disabled = ref(false)
+const load = async () => {
+  // 获取下一页的数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodsList.value = [...goodsList.value, ...res.result.items]
+  // 加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -37,12 +55,12 @@ const getGoodsData=async()=>{
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
         <GoodsItem v-for="good in goodsList" :good="good" :key="good.id"/>
       </div>
