6636e3682fbeef51768a09c68ae5355d83680429
Bascii098 <2021210202@bupt.edu.cn>
2024-07-01 01:41:27 +0800

    token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useUserStore } from '@/stores/user'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 // 创建axios实例
@@ -9,6 +10,13 @@ const http = axios.create({
 
 // axios请求拦截器
 http.interceptors.request.use(config => {
+    // 1. 从pinia获取token数据
+    const userStore = useUserStore()
+    // 2. 按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
   return config
 }, e => Promise.reject(e))
 
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,13 +1,14 @@
 <script setup>
-
+import { useUserStore } from '@/stores/user';
+const userStore =useUserStore()
 </script>
 
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
@@ -31,7 +32,7 @@
 
 <style scoped lang="scss">
 .app-topnav {
-  background: #e5e4e3;
+  background: #eae9e8;
   ul {
     display: flex;
     height: 53px;
@@ -40,7 +41,7 @@
     li {
       a {
         padding: 0 15px;
-        color: #999;
+        color: #333;
         line-height: 1;
         display: inline-block;
 
